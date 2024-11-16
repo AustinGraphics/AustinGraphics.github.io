50043ac374181c40de61816ccb53bbbe9b76ea5e
austin <austinxthomas@gmail.com>
2024-11-16 22:36:29 +0000

    moved everything out of timetable

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,6 @@ import dotenv from 'dotenv';
 dotenv.config(); // Load .env variables
 
 export default defineConfig({
-    root: 'timetable',  // Specify the folder where your index.html is located
-    build: {
-        outDir: '../dist',  // Output build to a separate 'dist' directory
-    },
     define: {
         'process.env': process.env, // Inject environment variables into the browser
     },
